8c5c094c994cd7a43009dd6a41d12c9101f49088
TopicsOnData <info@topicsondata.com>
2024-06-09 01:44:45 -0400

    Update hp.py
    Changed to accomodate for new plug-in

--- hp/hp.py
+++ hp/hp.py
@@ -7,33 +7,33 @@ import plotly.graph_objects as go
 import pandas as pd
 from pandas import Series, DataFrame
 
-df = pd.read_csv('properties.csv')
+df = pd.read_csv('properties-2.csv')
 
 # Draw the figure
 fig = go.Figure()
 
-# Drop any rows where the Living Area is blank
-df = df.dropna(subset=['Living area'])
+# Drop any rows where the square feet cell is blank
+df = df.dropna(subset=['sqft'])
 
 # Keep only Single-Family Homes (SFH)
-df = df.loc[df['Property type'] == 'Single Family']
+df = df.loc[df['homeType'] == 'SINGLE_FAMILY']
 
 # Reset index, drop old index
 df = df.reset_index(drop=True)
 
 # Add the data
-prices = df['Property price (USD)']
-square_feet = df['Living area']
+prices = df['price']
+square_feet = df['sqft']
 
 prices = prices.div(1000)
 
 # Add starting weight and bias
-w_init = 1.5e-1 # Increase in price for every 1 square feet
-b_init = 500 # Starting price for the cheapest houses
+w_init = 2.75e-1 # Increase in price for every 1 square feet
+b_init = 325 # Starting price for the cheapest houses
 
 # Iterations and learning rate for the gradient descent algorithm
 iterations = 10000
-alpha = 1.0e-6 # Delicate and causes a divergence if it's set too large
+alpha = 1.0e-9 # Delicate and causes a divergence if it's set too large
 
 w_final, b_final, J_hist, p_hist = gradient_descent(
     square_feet, prices, w_init, b_init, alpha, iterations)
