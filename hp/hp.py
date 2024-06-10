07df5d58a0fb965e6ba1a73b12533393af35c39e
TopicsOnData <info@topicsondata.com>
2024-06-09 20:45:08 -0400

    Update hp.py
    Does not consider outliers in the linear regression but includes them in the graph.

--- hp/hp.py
+++ hp/hp.py
@@ -7,7 +7,7 @@ import plotly.graph_objects as go
 import pandas as pd
 from pandas import Series, DataFrame
 
-df = pd.read_csv('properties-2.csv')
+df = pd.read_csv('properties-gables.csv')
 
 # Draw the figure
 fig = go.Figure()
@@ -18,22 +18,30 @@ df = df.dropna(subset=['sqft'])
 # Keep only Single-Family Homes (SFH)
 df = df.loc[df['homeType'] == 'SINGLE_FAMILY']
 
+# Remove outliers for the algorithm, but keep them for the graph
+prices_with_outliers = df['price']
+square_feet_with_outliers = df['sqft']
+df.drop(index=df.nlargest(4, 'price').index, inplace=True)
+
 # Reset index, drop old index
 df = df.reset_index(drop=True)
+square_feet_with_outliers = square_feet_with_outliers.reset_index(drop=True)
+prices_with_outliers = prices_with_outliers.reset_index(drop=True)
 
 # Add the data
 prices = df['price']
 square_feet = df['sqft']
 
 prices = prices.div(1000)
+prices_with_outliers = prices_with_outliers.div(1000)
 
 # Add starting weight and bias
-w_init = 2.75e-1 # Increase in price for every 1 square feet
-b_init = 325 # Starting price for the cheapest houses
+w_init = 1.6 # Increase in price for every 1 square feet
+b_init = -800 # Starting price for the cheapest houses
 
 # Iterations and learning rate for the gradient descent algorithm
 iterations = 10000
-alpha = 1.0e-9 # Delicate and causes a divergence if it's set too large
+alpha = 1.0e-8 # Delicate and causes a divergence if it's set too large
 
 w_final, b_final, J_hist, p_hist = gradient_descent(
     square_feet, prices, w_init, b_init, alpha, iterations)
@@ -41,15 +49,14 @@ w_final, b_final, J_hist, p_hist = gradient_descent(
 print(f'w: {w_final}, b: {b_final}, J_start: {J_hist}, p_hist: {p_hist}')
 
 f_wb = []
-for i in range(square_feet.size):
-    f_wb.append(w_final * square_feet[i] + b_final)
-
+for i in range(square_feet_with_outliers.size):
+    f_wb.append(w_final * square_feet_with_outliers[i] + b_final)
 
 # Plot Square feet vs. Price, in thousands
 fig.add_trace(
     go.Scatter(
-        x=square_feet, 
-        y=prices,
+        x=square_feet_with_outliers, 
+        y=prices_with_outliers,
         mode='markers',
         name='Actual'
     )
@@ -58,7 +65,7 @@ fig.add_trace(
 # Plot Linear Regression Model
 fig.add_trace(
     go.Scatter(
-        x=square_feet, 
+        x=square_feet_with_outliers, 
         y=f_wb,
         mode='lines',
         line=dict(color='red', width=5),
@@ -66,7 +73,8 @@ fig.add_trace(
     )
 )
 
-fig.update_xaxes(title_text="Square Feet", range=[square_feet.min(), square_feet.max()])
+fig.update_xaxes(title_text="Square Feet", range=[square_feet_with_outliers.min(),
+    square_feet_with_outliers.max()])
 fig.update_yaxes(title_text="Price (in USD, thousands)")
 
 fig.update_layout(
